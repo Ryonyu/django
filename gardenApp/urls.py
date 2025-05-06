9ab6fb87bd0dcf7cdeb9f6166893a95c2806274c
Ryo <ryo.snowflakes@gmail.com>
2025-05-06 20:23:27 +0900

    fix

--- gardenApp/urls.py
+++ gardenApp/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
-    path('', views.index, name='index'), 
-    path('notebook/', views.notebook_list, name='notebook_list'),
+    # path('', views.index, name='index'), 
+    path('', views.notebook_list, name='notebook_list'),
     path('notebook/create/', views.notebook_create, name='notebook_create'),
     path('notebook/edit/<int:id>/', views.notebook_edit, name='notebook_edit'),
     path('notebook/delete/<int:id>/', views.notebook_delete, name='notebook_delete'),
