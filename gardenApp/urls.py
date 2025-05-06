baece048830c1750c4e319a38c82ec1f8025388c
Ryo <ryo.snowflakes@gmail.com>
2025-05-06 20:45:12 +0900

    login

--- gardenApp/urls.py
+++ gardenApp/urls.py
@@ -2,11 +2,15 @@ from . import views
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     # path('', views.index, name='index'), 
     path('', views.notebook_list, name='notebook_list'),
     path('notebook/create/', views.notebook_create, name='notebook_create'),
     path('notebook/edit/<int:id>/', views.notebook_edit, name='notebook_edit'),
     path('notebook/delete/<int:id>/', views.notebook_delete, name='notebook_delete'),
+
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
