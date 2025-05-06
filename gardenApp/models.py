d2d4382a78b9fb8cbcba3c9ec2f3cfd59ef2ff1d
Ryo <ryo.snowflakes@gmail.com>
2025-05-06 19:58:02 +0900

    test

--- gardenApp/models.py
+++ gardenApp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class NoteBook(models.Model):
+    title = models.CharField(max_length=255)  # ノートブックのタイトル
+    content = models.TextField()  # ノートブックの内容
+
+    category = models.CharField(max_length=255,null=True,blank=True)  # ノートブックのタイトル
+
+    season = models.CharField(max_length=255,null=True,blank=True)  # ノートブックのタイトル
+    floor = models.CharField(max_length=255,null=True,blank=True)  # ノートブックのタイトル
+    area = models.CharField(max_length=255,null=True,blank=True)  # ノートブックのタイトル
+
+    latitude = models.FloatField(null=True)
+    longitude = models.FloatField(null=True)
+
+    image = models.ImageField(upload_to='output_images/',null=True, blank=True)  # 出力画像
+    image2 = models.ImageField(upload_to='output_images/',null=True, blank=True)  # 出力画像
+    memo = models.CharField(max_length=255,null=True,blank=True)  # ノートブックのタイトル
+
+    url = models.URLField(null=True,blank=True)  # URL
+    created_at = models.DateTimeField(auto_now_add=True)  # 作成日時
+    updated_at = models.DateTimeField(auto_now=True)  # 更新日時
