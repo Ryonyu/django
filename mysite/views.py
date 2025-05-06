bd81b428f276e91a828a6b13c29880c377e02b9b
Ryo <ryo.snowflakes@gmail.com>
2025-05-06 18:53:53 +0900

    テスト

--- mysite/views.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from django.core.paginator import Paginator
-from django.db.models import Count,Q
-from django.urls import reverse
-from django.shortcuts import get_object_or_404, redirect, render
-
-def home(request):
-    return render(request, '/templates/index.html')
