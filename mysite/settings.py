f18cd5a17badfe6cbefba1619fcc5fa7fa789e59
Ryo <ryo.snowflakes@gmail.com>
2025-05-06 20:15:53 +0900

    fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,7 +36,10 @@ ALLOWED_HOSTS = ["*"]
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
 # CSRF_TRUSTED_ORIGINS = ["*"]
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://django-server-production-9d3a.up.railway.app',
+    # 他に信頼するオリジンを追加
+]
 # Application definition
 
 INSTALLED_APPS = [
