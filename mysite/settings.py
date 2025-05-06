54464ab61dd1f32c721e22e121660d29f0565485
Ryo <ryo.snowflakes@gmail.com>
2025-05-06 19:44:24 +0900

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,9 +130,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+#STATIC_URL = 'static/'
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+
+STATIC_URL = '/static/'
+
+# 静的ファイルのルートディレクトリ設定（`collectstatic` を実行した際に使用）
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'gardenApp/static'),  # ここに静的ファイルを配置
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
